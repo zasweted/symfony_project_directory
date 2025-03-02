ab747bd1f8b5bb859d4fea96518cb2bd3dea524b
zasweted <zaswet@gmail.com>
2025-03-02 20:41:57 +0200

    initial commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+    #[Route('/')]
+    public function homepage()
+    {
+        
+        return new Response('Hello World!');
+    }
+}
