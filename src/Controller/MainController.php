ccdfd902d6f59aaefd223d6ebbc65bd182a42182
zasweted <zaswet@gmail.com>
2025-03-02 21:10:13 +0200

    homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,15 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage()
     {
-        
-        return new Response('Hello World!');
+        $starship_count = 456;
+
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickle',
+            'status' => 'Under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarships' => $starship_count,
+            'myShip' => $myShip,
+        ]);
     }
 }
